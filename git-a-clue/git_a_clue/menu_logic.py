3427aa401d026af2b6c1f69a7e7d393ffbba8284
Amber Falbo <amberannfalbo@gmail.com>
2021-02-20 11:48:32 -0800

    working out errors

--- git-a-clue/git_a_clue/menu_logic.py
+++ git-a-clue/git_a_clue/menu_logic.py
@@ -48,37 +48,64 @@ class Menu_Logic:
         
         
     def rules(self): 
-        rules = """
-            Solve the murder of John Cokos:
-            1. Roll the dice to move from room to room
+        print("RULESSSSSSS")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    #     rules = """
+    #         Solve the murder of John Cokos:
+    #         1. Roll the dice to move from room to room
             
-            2. Your practice and final whiteboards will ask for a suspect, a gadget, and a room
-                - You must move to a room to complete any whiteboard
-                        AND
-                - Whiteboards can only reference the room you are standing in
+    #         2. Your practice and final whiteboards will ask for a suspect, a gadget, and a room
+    #             - You must move to a room to complete any whiteboard
+    #                     AND
+    #             - Whiteboards can only reference the room you are standing in
             
-            3. Practice your whiteboarding skills
-                - You'll start the game with 6 cards to eliminate suspects, gadgets and rooms
-                - Use the cards and the front-end website to develop your algorithm
-                - Make eliminations by suggesting a suspect, a gadget, and the room you are in
-                - The TA will give you a hint if they have one
-                - Use the feedback to make corrections to your practice whiteboard
-                - You can practice as many times as you want
+    #         3. Practice your whiteboarding skills
+    #             - You'll start the game with 6 cards to eliminate suspects, gadgets and rooms
+    #             - Use the cards and the front-end website to develop your algorithm
+    #             - Make eliminations by suggesting a suspect, a gadget, and the room you are in
+    #             - The TA will give you a hint if they have one
+    #             - Use the feedback to make corrections to your practice whiteboard
+    #             - You can practice as many times as you want
             
-            4. Your final whiteboard
-                - For your final whiteboard, move to the room you'd like to solve in
-                - Schedule some time with an instructor and present your final solution 
-                - The instructor will give you feedback and a passing or failing grade
-                - The game ends once you receive your grade
-                - 
-            5. You've only got one chance at your final whiteboard, so study hard
-        """
-        # clear screen and re-display menu
-        print(rules)
-        time.sleep(5)
-        os.system('cls' if os.name == 'nt' else 'clear')
-        # need to check if it's thier 1st time - if so, will need to go to different menu
-        # menu()
+    #         4. Your final whiteboard
+    #             - For your final whiteboard, move to the room you'd like to solve in
+    #             - Schedule some time with an instructor and present your final solution 
+    #             - The instructor will give you feedback and a passing or failing grade
+    #             - The game ends once you receive your grade
+    #             - 
+    #         5. You've only got one chance at your final whiteboard, so study hard
+    #     """
+    #     # clear screen and re-display menu
+    #     print(rules)
+    #     time.sleep(5)
+    #     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 
 # testing function calls 
